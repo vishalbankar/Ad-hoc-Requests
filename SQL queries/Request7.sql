c45d14b3e79528e6db58ffb745a8a783dc6c6b47
Vishal Bankar <71269939+vishalbankar@users.noreply.github.com>
2023-11-07 20:00:59 +0530

    Update Request7.sql

--- SQL queries/Request7.sql
+++ SQL queries/Request7.sql
@@ -1,5 +1,5 @@
-Get the complete report of the Gross sales amount for the customer “Atliq
-Exclusive” for each month. This analysis helps to get an idea of low and
+--Get the complete report of the Gross sales amount for the customer “Atliq
+--Exclusive” for each month. This analysis helps to get an idea of low and
 -- high-performing months and take strategic decisions.
 -- The final report contains these columns:
 -- Month
