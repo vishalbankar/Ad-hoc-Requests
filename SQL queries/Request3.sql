b3f27001d52f88a45bca8ce048aed455e8b59fe3
Vishal Bankar <71269939+vishalbankar@users.noreply.github.com>
2023-11-07 19:57:41 +0530

    Delete SQL queries directory

--- SQL queries/Request3.sql
+++
@@ -1,8 +0,0 @@
--- 3.Provide a report with all the unique product counts for each segment and sort them in descending order of product counts. The final output contains 2 fields, 
--- segment product_count
-
-
-SELECT segment, COUNT(Distinct product_code)as product_cnt 
-FROM gdb023.dim_product
-group by segment
-order by product_cnt DESC ;
